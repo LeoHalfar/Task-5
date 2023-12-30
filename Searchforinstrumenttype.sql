47793f88bfc06ceeeb0e92d684686104d9efc82a
LeoHalfar <151862077+LeoHalfar@users.noreply.github.com>
2023-12-30 17:00:42 +0100

    Update Searchforinstrumenttype.sql

--- Searchforinstrumenttype.sql
+++ Searchforinstrumenttype.sql
@@ -4,8 +4,16 @@ SELECT
     ri.type_of_instrument
 FROM
     rentable_instruments ri
-LEFT JOIN
-    renting_instruments rin ON ri.rentable_instruments_id = rin.rentable_instruments_id
+LEFT JOIN (
+    SELECT
+        rentable_instruments_id,
+        COUNT(rentable_instruments_id) AS total_rented,
+        COUNT(expired) AS expired_count
+    FROM
+        renting_instruments
+    GROUP BY
+        rentable_instruments_id
+) rin_counts ON ri.rentable_instruments_id = rin_counts.rentable_instruments_id
 WHERE
-    ri.type_of_instrument = 'The instrument type'
-    AND (rin.expired IS NOT NULL OR rin.rentable_instruments_id IS NULL);
+    ri.type_of_instrument = 'Piano'
+    AND (rin_counts.rentable_instruments_id IS NULL OR rin_counts.total_rented = rin_counts.expired_count);
